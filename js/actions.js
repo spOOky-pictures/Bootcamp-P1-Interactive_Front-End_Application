980c21662ab1003d91258d2f9b9ef9e0f997ddb2
NukeTurtle <estiglic@edocuments.co.uk>
2023-02-01 14:35:40 +0000

    initial commit with base folder/file structure, linked bootstrap/jquery/jqueryUI/fontawesome and favicon image

---
+++ js/actions.js
@@ -0,0 +1,7 @@
+
+// !!! remove before writing actual code !!!
+console.log(`
+||||||||||||||||||||||||||||||||||||||||||||||||
+| It's not a bug; it's an undocumented feature |
+||||||||||||||||||||||||||||||||||||||||||||||||`
+)
