6bb85142dfa57f2a4d00d7db6b49374f2ddc1dcb
Emma Hayward <emmahayward93@gmail.com>
2023-02-01 20:45:27 +0000

    Added query selctors for search button and search value, added on click function to test value input

--- js/actions.js
+++ js/actions.js
@@ -1,7 +1,7 @@
 
-// !!! remove before writing actual code !!!
-console.log(`
-||||||||||||||||||||||||||||||||||||||||||||||||
-| It's not a bug; it's an undocumented feature |
-||||||||||||||||||||||||||||||||||||||||||||||||`
-)
+var searchButton = $("#searchBtn");
+var searchInput = $("searchValue");
+
+searchButton.on("click", function(){
+    console.log(searchInput.val());
+})
