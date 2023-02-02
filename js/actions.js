2d29757a11bb9c7ecf6975ff90b528dc523247bb
Emma Hayward <emmahayward93@gmail.com>
2023-02-02 14:29:23 +0000

    Take the users input from the search bar and send it to the Tasty API to return eight recipie IDs

--- js/actions.js
+++ js/actions.js
@@ -1,9 +1,29 @@
 // Selectors for search button
+//TODO - change id names HTML dependant
 var searchButton = $("#searchBtn");
-var searchInput = $("searchValue");
-var searchInputValue = searchInput.val();
+var searchInput = $("#searchValue");
 
 //On click function for search button
-searchButton.on("click", function(){
-    console.log(searchInput);
+searchButton.on("click", function(event){
+    event.preventDefault();
+    var userInput = searchInput.val().trim();
+//Tasty API Query one - list of meals
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tasty.p.rapidapi.com/recipes/list?from=0&size=8&q=Chicken",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "78e5609727msh1ba34c0ebc625dcp184ea7jsn9d56a3cb8dc5",
+            "X-RapidAPI-Host": "tasty.p.rapidapi.com"
+        }
+    };
+//Getting response from API for list of recipes that include ingredient
+    $.ajax(settings).done(function (response) {
+    //for loop to loop through responses and pull ID numbers
+        //TODO recipie more info API query will need to go in this loop.
+        for ( var i = 0; i < response.results.length; i++){
+            console.log(response.results[i].id);
+        }
+    });
 })
