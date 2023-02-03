69adfa03fe77f5def000dfc2f8873b93e517e0f7
Davvid96 <zajacek19@gmail.com>
2023-02-03 23:11:10 +0000

    added function for home button on click to reload the page

--- js/actions.js
+++ js/actions.js
@@ -133,3 +133,9 @@ $(".prevSearch").on("click", function(){
     });
 
 })
+
+// function for home button to refresh page and go back to original state
+$("#homeButton").on("click", function(){
+    document.location.reload()
+})
+
