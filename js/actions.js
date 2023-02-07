4d19f0419cfcdaadd56907b8955344e52570c23b
Davvid96 <zajacek19@gmail.com>
2023-02-07 18:59:36 +0000

    re-adding the home button

--- js/actions.js
+++ js/actions.js
@@ -143,7 +143,13 @@ $(".prevSearch").on("click", function(event){
 
 })
 
+
+
+
+
+
 // function for home button to refresh page and go back to original state
 $("#homeButton").on("click", function(){
     document.location.reload()
 })
+
